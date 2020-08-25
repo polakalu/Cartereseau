70215bbd2c456939564a91325bf13010493afdde
lapolakalu <titteringtonpaola@gmail.com>
2020-08-25 12:26:01 +0200

    OK
    :+1:
    
    Manquent :
    - cluster points
    - changer couleurs par filtres thèmes
    - add info popup
    - ajuster localisation et css de check
    - Requête dans la bar recherche: filter by name
    - localisation de l'utilisateur
    - link pour les sites internet
    - filtre par date

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ function createPopup(currentFeature) {
     if (popups[0]) popups[0].remove();
     const popup = new mapboxgl.Popup({ closeOnClick: true })
         .setLngLat(currentFeature.geometry.coordinates)
-        .setHTML("<h3>" + currentFeature.properties[config.popupName] + "</h3>"+"<h4>"" + "<b>" + "Description:" + "</b>" + currentFeature.properties[config.popupDescription] + "</h4>")
+        .setHTML("<h3>" + currentFeature.properties[config.popupInfo] + "</h3>")
         .addTo(map);
 }
 
