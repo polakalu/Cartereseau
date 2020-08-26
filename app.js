e251fb23c27f1cab24fdff6bf1cf907b671fa67d
lapolakalu <titteringtonpaola@gmail.com>
2020-08-26 12:35:41 +0200

    ok site web

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ function flyToLocation(currentFeature, zoom) {
 function createPopup(currentFeature) {
     const popups = document.getElementsByClassName("mapboxgl-popup");
     /** Text in the popup */
-    const informations =`<h3>` + currentFeature.properties[config.popupNom] + `</h3>`+`<h4>`+ currentFeature.properties[config.popupDescrip] + `</h4>`+ `<h4>` + `<b>` + currentFeature.properties[config.popupWeb] +`</b>` + `</h4>`;
+    const informations =`<h3>` + currentFeature.properties[config.popupNom] + `</h3>`+`<h4>`+ currentFeature.properties[config.popupDescrip] + `</h4>`+ `<h4>` + currentFeature.properties[config.popupWeb] + `</h4>`;
 
     /** Check if there is already a popup on the map and if so, remove it */
     if (popups[0]) popups[0].remove();
