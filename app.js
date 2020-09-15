6d906d47093b3a61ec6401fe93ee659d48b5d4b0
lapolakalu <titteringtonpaola@gmail.com>
2020-09-15 18:26:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -456,7 +456,7 @@ map.on("load", function () {
                                 ],
                     "circle-stroke-color": "grey",
                     "circle-stroke-width": 1,
-                    "circle-opacity": 0.9
+                    "circle-opacity": 0.7
                 }
 
             });
