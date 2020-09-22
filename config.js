3c510468e2941a32ff29e6dd896fe98f464a43a7
lapolakalu <titteringtonpaola@gmail.com>
2020-09-22 11:34:23 +0200

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const config = {
     sideBarInfo: ["Structure"],
     popupNom: ["Structure"],
     popupDescrip:["Description"],
-    popupWeb:["Siteweb"],
+    popupWeb:["Site web"],
     filters: [
         {
             type: "checkbox",
